27e417c3464110e9f4742014da1cff65ad9f9f0a
Arsene Awounou <developer.awounou@gmail.com>
2022-08-05 18:20:08 +0100

    user account and profile picture

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Handling Forms"""
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import (
     StringField, PasswordField, SubmitField, BooleanField
 )
@@ -41,3 +43,29 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class UpdateAccountForm(FlaskForm):
+    """Registration form handler"""
+    username = StringField('Username', validators=[
+        DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[
+        DataRequired(), Email()])
+    picture = FileField(
+        'Update profile picture', validators=[FileAllowed(['jpg', 'png'])]
+    )
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+
+            if user:
+                raise ValidationError('Username exists. Please choose another one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+
+            if user:
+                raise ValidationError('Email exists. Please choose another one.')
